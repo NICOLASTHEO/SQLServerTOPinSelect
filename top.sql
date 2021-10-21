2a8b67bbec36248b0fceb5f58d82ed926a1255b3
THEO NICOLAS GOMES DE ARAUJO <theonicolas87@gmail.com>
2021-10-21 09:33:43 -0300

    cmd TOP to specify qtt of lines to work

---
+++ top.sql
@@ -0,0 +1,13 @@
+/* TOP
+
+TOP delimita o número de itens que aparecem em uma busca SELECT.
+O SELECT busca todos os itens do critério passado (pode ser milhares), com o TOP especificamos o total q queremos.
+*/
+
+EX:
+
+SELECT TOP 100 *
+FROM TABELA QUE SE QUER TRABALHAR
+
+SELECT TOP 10 *
+FROM production.product
